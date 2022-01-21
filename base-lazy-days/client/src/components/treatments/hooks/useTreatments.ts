539606c0783d4dba43113cd8269dbdc87eb9110c
bgarcia95 <bgarcia.95@outlook.com>
2022-01-21 13:36:53 -0600

    fetch and prefetch treatments data

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,25 @@
+import { useQuery, useQueryClient } from 'react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  const fallback = [];
+
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+
+  return data;
+}
+
+export function usePrefetchTreatments(): void {
+  const queryClient = useQueryClient();
+  // they query key we set here has to be the same one for the 'useQuery' we want to prefetch the data on
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
 }
